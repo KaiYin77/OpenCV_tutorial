3f1d69337a202a16a8ea82341f74dd9e2f03811b
Kevin Hong <kevin880208@gmail.com>
2021-06-15 23:54:05 +0800

    add video reading part

--- read.py
+++ read.py
@@ -1,7 +1,23 @@
 import cv2 as cv
 
-img = cv.imread('Resources/Photos/cat.jpg')
+# Reading Image
+# img = cv.imread('Resources/Photos/cat_large.jpg')
 
-cv.imshow('Cat', img)
+# cv.imshow('Cat', img)
 
-cv.waitKey(0)
+# cv.waitkey(0)
+
+# Reading Videos
+# capture = cv.VideoCapture(0) # Reference the webcam
+capture = cv.VideoCapture('Resources/Videos/dog.mp4')
+
+while True:
+    isTrue, frame = capture.read()
+    
+    cv.imshow('Video', frame)
+    
+    if cv.waitKey(20) & 0xFF==ord('d'):
+        break
+
+capture.release()
+cv.destroyAllWindows()
