195bde78607e134d6d7833502f027b343691cf83
Kevin Hong <kevin880208@gmail.com>
2021-06-16 12:44:26 +0800

    add resize function

--- read.py
+++ read.py
@@ -1,11 +1,18 @@
 import cv2 as cv
 
+'''
 # Reading Image
-# img = cv.imread('Resources/Photos/cat_large.jpg')
+img = cv.imread('Resources/Photos/cat_large.jpg')
+cv.imshow('Cat', img)
+#cv.waitkey(0)
+'''
 
-# cv.imshow('Cat', img)
-
-# cv.waitkey(0)
+def rescaleFrame(frame, scale=0.75):
+    width = int(frame.shape[1] * scale)
+    height = int(frame.shape[0] * scale)
+    dimensions = (width, height)
+    
+    return cv.resize(frame, dimensions, interpolation=cv.INTER_AREA)
 
 # Reading Videos
 # capture = cv.VideoCapture(0) # Reference the webcam
@@ -14,7 +21,10 @@ capture = cv.VideoCapture('Resources/Videos/dog.mp4')
 while True:
     isTrue, frame = capture.read()
     
+    frame_resized = rescaleFrame(frame, .5)
+
     cv.imshow('Video', frame)
+    cv.imshow('Video_resized', frame_resized)
     
     if cv.waitKey(20) & 0xFF==ord('d'):
         break
