8802ae1dcddbdc91ccaf72151fd079bb8cc1a008
Kevin Hong <kevin880208@gmail.com>
2021-06-16 12:57:52 +0800

    add change resolution function

--- read.py
+++ read.py
@@ -1,18 +1,27 @@
 import cv2 as cv
 
-'''
 # Reading Image
-img = cv.imread('Resources/Photos/cat_large.jpg')
-cv.imshow('Cat', img)
-#cv.waitkey(0)
-'''
+# img = cv.imread('Resources/Photos/cat_large.jpg')
+# cv.imshow('Cat', img)
+# cv.waitkey(0)
+
 
 def rescaleFrame(frame, scale=0.75):
+    # Image , Video, Camera so on
     width = int(frame.shape[1] * scale)
     height = int(frame.shape[0] * scale)
     dimensions = (width, height)
     
     return cv.resize(frame, dimensions, interpolation=cv.INTER_AREA)
+
+# Resize image
+#resized_image = rescaleFrame(img, .25)
+#cv.imshow('Image_resize', resized_image)
+
+def changeRes(width, height):
+    # Only for Live video
+    capture.set(3, width) #capture.get(3) will get the width of the frame in the video stream
+    capture.set(4, height) 
 
 # Reading Videos
 # capture = cv.VideoCapture(0) # Reference the webcam
